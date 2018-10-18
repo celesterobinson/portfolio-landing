da276518b35e39c3b273c205d248cfd2434ba4f5
Celeste Robinson <crobinso@adobe.com>
2018-10-18 14:22:42 -0600

    changed text

--- src/App.js
+++ src/App.js
@@ -9,16 +9,14 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            You've hit my landing page. Learn more about cececodes below.
+            This page could be yours.
           </p>
-          <a
+          <p>Click<a
             className="App-link"
             href="https://www.cececodes.com"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Visit Cece
-          </a>
+          > here</a> for a free consultation, <br/>and get your website started TODAY!</p>
         </header>
         <footer>
             <div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
