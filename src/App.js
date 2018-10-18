5fbe8f8f3b0f06f961b5ef92747d8c17755b10a0
Celeste Robinson <crobinso@adobe.com>
2018-10-18 14:36:38 -0600

    updated icon author credit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           > here</a> for a free consultation, <br/>and get your website started TODAY!</p>
         </header>
         <footer>
-            <div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
         </footer>
       </div>
     );
