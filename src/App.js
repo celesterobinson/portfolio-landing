cfcd1739f8775b04f1c66cc5053ce894b5382f81
Celeste Robinson <crobinso@adobe.com>
2018-10-18 13:49:37 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            You've hit my landing page. Learn more about cececodes below.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.cececodes.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Visit Cece
           </a>
         </header>
+        <footer>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        </footer>
       </div>
     );
   }
